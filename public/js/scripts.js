ff604c3e2ede4f3d86dfa22abef6f0ca08e20385
Jon Gamble <jgamble@JonGamble.local>
2014-08-13 00:17:02 -0400

    version 0.3.1

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -20,20 +20,3 @@ if("undefined"==typeof jQuery)throw new Error("Bootstrap's JavaScript requires j
 	})
 
 }(this, this.document, this.jQuery));
-
-// public/core.js
-var appFunctions = angular.module('appFunctions', []);
-
-function lightsController($scope, $http) {
-	$scope.formData = {};
-
-	// when landing on the page, get all todos and show them
-	$http.get('/admin/listLights')
-		.success(function(data) {
-			$scope.lights = data;
-			//console.log(data);
-		})
-		.error(function(data) {
-			console.log('Error: ' + data);
-		});
-};
