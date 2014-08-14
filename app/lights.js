4e517cb8e504ff022c86c4c03b8ee97ac756ea1a
Jon Gamble <jgamble@JonGamble.local>
2014-08-13 23:48:24 -0400

    Version 0.4.0

--- app/lights.js
+++ app/lights.js
@@ -4,18 +4,18 @@ var Lights	=	require('./models/lights')
 
 module.exports = function(app, mongoose, gpio) { 
 	// set routes
-    app.get('/lights', function(req, res) {
-      Lights.find().exec(function(err, items){
+	app.get('/lights', function(req, res) {
+		Lights.find().exec(function(err, items){
 			res.render('lights', {
 				items:items //get light data and pass to template
 			});
-      });
-    });
+		});
+	});
 
-    app.get('/lights/listLights', function(req, res){
+	app.get('/lights/listLights', function(req, res){
 		Lights.find().exec(function (err, items) {
 			res.json(items);
-	   	});
+		});
 	});
 
 	app.get('/lights/lightSwitch/:id', function(req,res){
@@ -24,19 +24,20 @@ module.exports = function(app, mongoose, gpio) {
 			light.status = !light.status;
 			console.log(light);
 			gpio.setup(11, gpio.DIR_OUT, write);
+			gpio.destroy(function() {
+				console.log('Closed pins');
+				return;
+			});
 			function write(){
 				gpio.write(11, true, function(err){
 					if(err) throw err;
 					console.log('Written to pin');
 				});
-				gpio.destroy(function() {
-		            console.log('Closed pins');
-		            return;
-		        });
+
 			}
 			light.save(function(err){
 				if (err) console.log(err);
-			    res.redirect('/lights');
+				res.redirect('/lights');
 			});
 		});
 	});
