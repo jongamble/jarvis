22797814341fd4c27932ea6a8dbc33e00cb25311
Jon Gamble <jon@centerline.net>
2014-08-18 23:20:50 -0400

    version 0.4.3

--- public/js/core.js
+++ public/js/core.js
@@ -1,7 +1,7 @@
 // public/js/core.js
 var appFunctions = angular.module('appFunctions', []);
 
-function lightsController($scope, $http, socket) {
+function lightsController($scope, $http) {
 	$scope.formData = {};
 
 	// when landing on the page, get all todos and show them
